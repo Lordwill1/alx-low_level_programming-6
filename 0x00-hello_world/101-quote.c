2f14c355003e58f8b9c699f8622025fbc6698108
attahchisom6 <111227733+attahchisom6@users.noreply.github.com>
2022-09-09 01:57:14 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+
 /**
 *main-exec staring point
 *Return:1
@@ -8,6 +9,7 @@
 int main(void)
 {
 	int l = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	write(2, "and that piece of art is useful\" - Dora Korpar,2015-10-19\n", l);
+	write(2, 
+"and that piece of art is useful\" - Dora Korpar,2015-10-19\n", l);
 	return (1);
 }
