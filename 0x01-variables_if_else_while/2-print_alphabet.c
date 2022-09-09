2d2f14a8ff84d85ff0627a39f99998e863a53ea6
attahchisom6 <111227733+attahchisom6@users.noreply.github.com>
2022-09-09 20:33:37 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main-function main is exetion entry point
- * Return: 0
- */
+* main-function main is exetion entry point
+* Return: 0
+*/
 
 int main(void)
 {
-	int n;
+	char = lowercase;
 	
-	srand(time(0));
-	n = rand() - RAND_MAX \ 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else
-		printf("%d is negative\n", n);
+	
+	lowercase = 'a';
+	do {
+		putchar("lowercase\n");
+	lowercase++;
+		} while (a <= z);
+putchar("\n");
 	return (0);
 }
