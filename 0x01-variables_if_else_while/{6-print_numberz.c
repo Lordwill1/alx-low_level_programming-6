4e86116a8b35aec6826d517d8615b2bf50c73ca9
attahchisom6 <111227733+attahchisom6@users.noreply.github.com>
2022-09-09 23:32:30 +0100

    Update and rename {6-print_numbers.c to {6-print_numberz.c

---
+++ 0x01-variables_if_else_while/{6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main-function main is exetion entry point
+* Return: 0
+*/
+
+int main(void)
+{
+	int n;
+	
+	for (n = 0; n <= 9; n++)
+{
+	putchar(n);
+}
+		putchar('\n');
+	return (0);
+}
