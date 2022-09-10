941bb6346fa08cc34e2c04db160d81b20dd2ff98
attahchisom6 <111227733+attahchisom6@users.noreply.github.com>
2022-09-10 02:35:22 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int n, m;
 	
-	for (n = 48; n <= 57; n++)
+	for (n = 48; n <= 56; n++)
 {
-	for (m = 48; m <= 57; m++)
+	for (m = 49; m <= 57; m++)
 {
 	putchar(n);
 putchar(m);
-if (m < 57)
+if (m < 57 && n < 56)
 {
 		putchar(44);
 putchar(32);
