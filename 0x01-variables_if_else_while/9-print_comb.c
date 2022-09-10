0408636b903163a833ece3ae275b1fac177fb21f
attahchisom6 <111227733+attahchisom6@users.noreply.github.com>
2022-09-10 01:12:03 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main-function main is exetion entry point
- * Return: 0
- */
+* main-function main is exetion entry point
+* Return: 0
+*/
 
 int main(void)
 {
-	int n;
+	int n, j, k;
 	
-	srand(time(0));
-	n = rand() - RAND_MAX \ 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else
-		printf("%d is negative\n", n);
+	while (n = 48 && j = 44 && k = 32)
+{
+
+	putchar(njk);
+	
+		n++;
+}
+	putchar('\n');
+		
 	return (0);
 }
