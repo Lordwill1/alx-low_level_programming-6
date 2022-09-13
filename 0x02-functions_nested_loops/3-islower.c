35c2a295ae4a9aa9b904d582528b91f9b09bb459
attahchisom6 <attahchisom6@gmail.com>
2022-09-13 14:00:30 -0700

    detecting lower cases

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower-execution entry point
+ * Return:1 is if loqer is inputed otherwise 0 is returned
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
