96d9b476c0aef0a330b32932fbb3b00eb918650d
attahchisom6 <attahchisom6@gmail.com>
2022-09-13 14:41:31 -0700

    finding  alphabets of all cases

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha-execution entru point
+ * @c: this evaluate the given data inorder to return aresult
+ * Return:1 if success or 0 if failure
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c <= 'A' && c <= 'Z'));
+}
