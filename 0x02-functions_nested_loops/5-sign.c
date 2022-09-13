4f58a48fd8816558b296817b5b9b5ebb253ccb5d
attahchisom6 <attahchisom6@gmail.com>
2022-09-13 15:40:38 -0700

    checking for signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign-entry point
+ * @n: this checks numbers passed to the programm
+  Return:1 or -1 or 0 dependinding on the  interger sign
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		-putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (0);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
