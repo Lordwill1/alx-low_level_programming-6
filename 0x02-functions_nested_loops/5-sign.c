6ff6855192da0f466ae579c768210aa7f28e4c40
attahchisom6 <111227733+attahchisom6@users.noreply.github.com>
2022-09-14 00:01:31 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,9 +3,9 @@
 /**
  * print_sign-entry point
  * @n: this checks numbers passed to the programm
-  Return:1 if n is positive
- * -1 if n is negative
- * 0 if n is zero
+  Return:1 if n is positive and print +
+ * -1 if n is negative and print -
+ * 0 if n is zero and print 0
  */
 
 int print_sign(int n)
@@ -18,7 +18,7 @@ int print_sign(int n)
 	else if (n < 0)
 	{
 		_putchar('-');
-		return (0);
+		return (-1);
 	}
 	else
 	{
