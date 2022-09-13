be9990633c979347f61b0f7824c5947af69be16b
attahchisom6 <attahchisom6@gmail.com>
2022-09-13 11:46:48 -0700

    listing it 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main2.h"
+
+/**
+*print_alphabet_x10-prints the alphabets 10x
+*Return:0
+*/
+
+void print_alphabet_x10(void)
+{
+	int k;
+	char lwr;
+
+	for (k = 1; k <= 10; k++)
+	{
+		for (lwr = 'a'; lwr <= 'z'; lwr++)
+		{
+			_putchar(lwr);
+		}
+		_putchar('\n');
+	}
+}
