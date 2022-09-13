2d4ee46e1959354e370f3b6639bd96a5f46dc837
attahchisom6 <attahchisom6@gmail.com>
2022-09-13 09:26:47 -0700

    printing lower case alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#define MAXSTRING 80
+/**
+ * main-function main
+ * Return:0
+ */
+
+int main(void)
+{
+	int k;
+	char lwr[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (k = 0; k < MAXSTRING; k++)
+	{
+		if (lwr[k] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		else
+			_putchar(lwr[k]);
+	}
+	return (0);
+}
