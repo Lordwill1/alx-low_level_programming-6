2077e39e053653d2f5b79f48c0344ad959df206f
attahchisom6 <attahchisom6@gmail.com>
2022-09-14 15:51:28 +0100

    first 50 finonaci series

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main-To find all term of the finonacci series, up to the 50th term
+ * Return:0
+ */
+
+int main(void)
+{
+	long int m, n, p, next_term;
+
+	m = 1;
+	n = 2;
+	
+	for (p = 1; p <= 50; p++)
+	{
+		if (m != 2036501104)
+		{
+			printf("%ld, ", m);
+		}
+		else
+		{
+			printf("%ld\n", m);
+		}
+		next_term = m + n;
+		m = n;
+		n = next_term;
+	}
+	return (0);
+}
+
