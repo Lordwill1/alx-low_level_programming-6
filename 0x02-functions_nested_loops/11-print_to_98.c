d9029196e6ca6a02df0ad7b262e355d28ca98d0d
attahchisom6 <111227733+attahchisom6@users.noreply.github.com>
2022-09-14 04:56:54 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-				printf("%d", i);
+				printf("%d, ", i);
 			else if (i == 98)
 				printf("%d\n", i);
 		}
@@ -26,7 +26,7 @@ void print_to_98(int n)
 			for (j = n; j >= 98; j--)
 			{
 				if (j != n)
-					printf("%d", j);
+					printf("%d, ", j);
 				else if (j == n)
 					printf("%d\n", j);
 			}
