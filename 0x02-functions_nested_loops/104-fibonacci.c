e29cd7636657f0c2bd6388037d8f086de68b88e6
attahchisom6 <attahchisom6@gmail.com>
2022-09-15 00:04:41 +0100

    98th sequence of fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main-execution entry point
+ * Return:0
+ */
+
+int main()
+{
+	unsigned long int m, n, m1, m2, n1, n2, p;
+
+	m = 1;
+	n = 2;
+	printf("%lu", m);
+
+	for (p = 1; p < 97; p++)
+	{
+		printf(", %lu", n);
+		
+		n = n + m;
+		m = n - m;
+	}
+	m1 = m / 1000000000;
+	m2 = m % 1000000000;
+	n1 = n / 1000000000;
+	n2 = n % 1000000000;
+	
+	for (p = 92; p <= 99; ++p)
+	{
+		printf(", %lu", n1 + (n2 /1000000000));
+		printf("%lu", n2 % 1000000000);
+		n1 = n1 + m1;
+		m1 = n1 - m1;
+		n2 = n2 + m2;
+		m2 = n2 - m2 ;
+	}
+	printf("\n");
+	return (0);
+}
