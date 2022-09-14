a83cfcb564ca7adfd63684ed7225e2bab6b19d2d
attahchisom6 <attahchisom6@gmail.com>
2022-09-13 20:45:03 -0700

    count to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_to_98-print natural numbers to 98
+ * @n:running int
+ */
+
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	}
+
+		else if (n >= 98)
+		{
+			for (j = n; j >= 98; j--)
+			{
+				if (j != n)
+					printf("%d", j);
+				else if (j == n)
+					printf("%d\n", j);
+			}
+		}
+}
