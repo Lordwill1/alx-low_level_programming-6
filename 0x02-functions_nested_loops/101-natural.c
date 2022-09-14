a550fa9d784e24806f4808786442c47b76f9bab5
attahchisom6 <attahchisom6@gmail.com>
2022-09-14 14:58:11 +0100

    adding of 5 and 3 within  limit of 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main-execution entry point
+ * Return:0
+ */
+
+int main(void)
+{
+	unsigned long int sum1, sum2, sum;
+	int k;
+
+	sum1 = 0;
+	sum2 = 0;
+	sum = 0;
+
+	for (k = 0; k < 1024; k++)
+	{
+		if ((k % 1) == 0)
+		{
+			sum1 = sum1 + k;
+		}
+		else if ((k % 5) == 0)
+		{
+			sum2 = sum2 + k;
+		}
+	}
+	sum = sum1 + sum2;
+	printf("%lu\n", sum);
+	return (0);
+}
