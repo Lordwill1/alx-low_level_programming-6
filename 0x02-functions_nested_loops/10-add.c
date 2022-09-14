ff2b3196fd2be103cef8a1f1319182b67c997c9d
attahchisom6 <attahchisom6@gmail.com>
2022-09-13 20:01:39 -0700

    adding

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add-entry point of  funtion
+ * @n: search or integer arguments
+ * @m: another integer yo be added
+ * Return:int
+ */
+
+int add(int n, int m)
+{
+	int ret;
+
+	ret = n + m;
+	return (ret);
+}
