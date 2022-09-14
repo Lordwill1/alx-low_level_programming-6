8f856da26262b91798fc8a8793e9d18d18ac1d43
attahchisom6 <attahchisom6@gmail.com>
2022-09-13 17:03:30 -0700

    the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit-execution entry point
+ * @n: looks for integer data type
+ * Return:int
+ */
+
+int print_last_digit(int n)
+{
+	int ret;
+
+	ret = n % 10;
+	return (ret);
+}
