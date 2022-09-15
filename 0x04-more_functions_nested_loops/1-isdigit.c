75bab60dc0af9f23c4d8b9202d8f4397dcebe78e
attahchisom6 <attahchisom6@gmail.com>
2022-09-15 12:14:57 +0100

    finding digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+include "main.h"
+
+/**
+ * _isdigit-function to search for digit
+ * @c:parameter to check for digit
+ * Return:int data type
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
