e86732ca66d4960d7044096510d1f21c39acf7d9
attahchisom6 <attahchisom6@gmail.com>
2022-09-15 11:41:36 +0100

    checking for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper-check for uppercase and returns 1 if any
+ * @c:checks for character argument
+ * Return:intcdata type
+ */
+
+_isupper(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
