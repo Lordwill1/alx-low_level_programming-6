f52b300eb92a0d392e5e25a043c30cff1e5fcccc
attahchisom6 <attahchisom6@gmail.com>
2022-09-15 22:17:37 +0100

    printing lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,24 @@
 #include "main.h"
 
 /**
-* print_line-print number of lines
-*@n:integer parameter
-*/
+ * * print_line - print number of lines
+ * @n:integer parameter
+ */
 
 void print_line(int n)
 {
-	int k;
-
 	if (k <= 0)
+	{
 		_putchar('\n');
-	for (k = 0; k < n; k++)
-		_putchar('_');
-	_putchar('\n');
+	}
+	else
+	{
+		int k;
+
+		for (k = 1; k <= n; k++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
