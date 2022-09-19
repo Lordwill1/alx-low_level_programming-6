ebb440aacfaadd7048408a138116ae9c09ff0a7e
attahchisom6 <attahchisom6@gmail.com>
2022-09-19 16:09:46 +0100

    my own puts function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - A function that will print a string a string
+ * to standard output
+ * @str:string parameter to check out  string
+ * to print out
+ */
+
+void _puts(char *str)
+{
+	int k;
+	
+	for (k = 0; str[k] != '\0'; k++)
+	{
+		putchar(str[k]);
+	}
+		putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ int _putchar(char c);
 void reset_to_98(int *n);
 void swap_(int *a, int *b);
 int _strlen(char *greeting);
+void _puts(char *str);
 
 #endif
