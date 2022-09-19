a5d4019792659de0f61b240b51b13f5ac516bddf
attahchisom6 <attahchisom6@gmail.com>
2022-09-19 12:01:11 +0100

    printing only 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function that any inputed number to 98
+ * @n: integer argument
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# **POINTERS AND ARRAYS IN C**
+
+_Main and complicated programs in are often done pointers, arrays and fumctions_
