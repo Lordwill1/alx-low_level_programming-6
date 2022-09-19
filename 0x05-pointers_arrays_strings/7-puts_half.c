f1abd2c8338f4bd7b1e533df8c7ed7ce4b7ea8cb
attahchisom6 <attahchisom6@gmail.com>
2022-09-19 21:45:09 +0100

    printing half the number

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * puts_half - A function that will print the 
- * second half of a given list
+ * puts_half - A function that will
+ * print second half of a given list
  * as well as the last number if odd
  * @str:string parameter
  */
@@ -13,14 +13,14 @@ void puts_half(char *str)
 	int p, n, k;
 
 	p = 0;
-	while (str[p] != 0)
+	while (str[p] != '\0')
 	{
 		p++;
 	}
-	
+
 	if (p % 2 == 0)
 	{
-		for (k = p / 2; str[k] != 0; k++)
+		for (k = p / 2; str[k] != '\0'; k++)
 		{
 			_putchar(str[k]);
 		}
@@ -33,4 +33,5 @@ void puts_half(char *str)
 			_putchar(str[n + 1]);
 		}
 	}
+	_putchar('\n');
 }
