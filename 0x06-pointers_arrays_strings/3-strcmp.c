b40b4619e758d78e5722334b882a2ca92fa68d60
attahchisom6 <attahchisom6@gmail.com>
2022-09-21 18:50:09 +0100

    comparing two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,14 @@
 
 int_strcmp(char *s1, char *s2)
 {
-int k, p;
+	int k, p;
 
-k = 0;
-while (s1[k] == s2[k] && s1[k] != '\0')
-{
+	k = 0;
+	while (s1[k] == s2[k] && s1[k] != '\0')
+	{
 	k++;
-}
+	}
 
-p = s1[k] - s2[k];
-return (p);
+	p = s1[k] - s2[k];
+	return (p);
 }
