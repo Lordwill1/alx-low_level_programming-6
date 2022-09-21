5157e3a0a245dce02aef793f09166f463beaecb5
attahchisom6 <attahchisom6@gmail.com>
2022-09-21 22:14:16 +0100

    capitalizing

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *cap_string - A function that capitalizes every
+ * character it runs on
+ * @s:string variable
+ * Return:result
+ */
+
+*cap_string(char *s)
+{
+	int k, p;
+
+	p = 0;
+	while (s[p] != '\0')
+	{
+		if (s[0] >= 'a' && s[0] <= 'z')
+		{
+			s[0] = s[0] - 32;
+		}
+
+		char arr[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+		for (k = 0; k < 13; k++)
+		{
+			if (s[p] = arr[k])
+			{
+				if (s[p + 1] >= 'a' && s[p] <= 'z')
+				{
+					s[p + 1] = s[p + 1] - 32;
+				}
+			}
+		}
+		p++;
+	}
+	return (s);
+}
