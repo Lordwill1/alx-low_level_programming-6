269c78cb9cd4cdc49cdc73db37067a43dbd487fd
attahchisom6 <attahchisom6@gmail.com>
2022-09-22 03:53:16 +0100

    good

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,17 +8,19 @@
 
 void print_number(int n)
 {
-	unsigned int m;
+	unsigned int n1;
 
-	m = n;
 	if (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		m = -n;
 	}
-	if (m / 10)
+	else
 	{
-		print_number(m / 10);
+		n1 = n;
 	}
-	_putchar((m % 10) + '0');
+
+	if (n1 / 10)
+		print_number(n1 / 10);
+	_putchar((n1 % 10) + '0');
 }
