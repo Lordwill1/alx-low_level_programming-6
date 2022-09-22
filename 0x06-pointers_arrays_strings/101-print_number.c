b89ddf1cffa861672748bad6a303a6dd2895d345
attahchisom6 <attahchisom6@gmail.com>
2022-09-22 03:37:07 +0100

    mumbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,16 +10,15 @@ void print_number(int n)
 {
 	unsigned int m;
 
+	m = n;
 	if (n < 0)
 	{
+		_putchar('-');
 		m = -n;
-		_putchar(45);
-	}
-	else
-	{
-		m = n;
 	}
 	if (m / 10)
+	{
 		print_number(m / 10);
+	}
 	_putchar((m % 10) + '0');
 }
