2882b5c6196752b15a956165c1792a11d5deee2b
attahchisom6 <attahchisom6@gmail.com>
2022-09-22 05:04:32 +0100

    marvelous

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -11,6 +11,7 @@ int main(void)
    /*
     * write your code here...
     * Remember:
+    * - you are not allowed to use a
     * - you are not allowed to modify p
     *   - only one statement
     *   - you are not allowed to code anything else than this line of code
